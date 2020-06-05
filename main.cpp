01289d408e33c5ac0d8aea2a67b55d159e873037
Carlos <cpenas@ic-itcr.ac.cr>
2020-06-04 20:08:24 -0600

    analisis de tipos de lineas

--- main.cpp
+++ main.cpp
@@ -8,33 +8,28 @@ using namespace std;
 
 typedef vector<pair<pair<int,int>,pair<int,int>>> estructuraDePuntos;
 typedef pair<estructuraDePuntos,estructuraDePuntos> puntosSeparados;
+class resultadosDeAnalisis {
+    public:
+    int diagonalesCrecientes = 0;
+    int diagonalesDecrecientes = 0;
+    int verticales = 0;
+    int horizontales = 0;
+    int cantidadTotal = 0;
 
-//retorna una lista con solo los puntos del cuadrante definido por los limites (no funciona bien todavia)
-puntosSeparados extraerCuadrante(int inferiorX, int superiorX, int inferiorY, int superiorY,puntosSeparados* puntos){
-    puntosSeparados resultado;
-    int contadorH = 0;
-    int contadorV = 0;
-    bool horizontalHecho = false;
-    pair<pair<int,int>,pair<int,int>> actual;
-    while(actual.first.first >= inferiorX && actual.first.first < superiorX && actual.first.second >= inferiorY && actual.first.second < superiorY){
-        //cout << "(" << actual.first.first << "," << actual.first.first << ")" << endl;
-        if(!horizontalHecho){
-            actual = puntos->first.at(contadorH);
-            resultado.first.push_back({actual.first,actual.second});
-            puntos->first.erase(puntos->first.begin() + contadorH);
-            if(contadorH == puntos->first.size()-1){
-                horizontalHecho = true;
-            }
-            contadorH++;
-        }
-        else{
-            actual = puntos->second.at(contadorV);
-            resultado.second.push_back({actual.first,actual.second});
-            puntos->second.erase(puntos->second.begin() + contadorV);
-            contadorV++;
-        }
+    void calcularTotales(){
+        cantidadTotal = diagonalesCrecientes + diagonalesDecrecientes + verticales + horizontales;
     }
-    return resultado;
+};
+const int margenDeError = 5;
+
+int generarRandom(int min,int max){
+    static bool first = true;
+    if (first) 
+    {  
+       srand( time(NULL) );
+       first = false;
+    }
+    return min + rand() % ((max) - min);
 }
 
 bool static comparar(pair<pair<int,int>,pair<int,int>> a, pair<pair<int,int>,pair<int,int>> b){
@@ -50,6 +45,10 @@ bool static comparar(pair<pair<int,int>,pair<int,int>> a, pair<pair<int,int>,pai
     else{
         return (a.first.second < b.first.second);
     }
+}
+
+bool static compararNormal(int a, int b){
+    return (a < b);
 }
 
 //separa los puntos en dos estructuraDePuntos, una con los puntos que salen del eje X (Horizontales) y otra del eje Y (Verticales)
@@ -73,17 +72,93 @@ puntosSeparados separarPuntos(estructuraDePuntos puntos){
 }
 
 //funcion principal
-void analizar(int cuadrantesX, int cuadrantesY,estructuraDePuntos puntos){
-    int cantidadLineas = puntos.size();
-    int saltoX = 1080/cuadrantesX;
-    int saltoY = 920/cuadrantesY;
-    int limiteInferiorX = 0;
-    int limiteSuperiorX = saltoX;
-    int limiteInferiorY = 0;
-    int limiteSuperiorY = saltoY;
-    for(int contador = 0; contador < cuadrantesX*cuadrantesY; contador++){
-
+resultadosDeAnalisis analizar(puntosSeparados puntosSep,float porcentajeAnalisis,int cantLineas){
+    resultadosDeAnalisis resultado;
+    int cantidadLineasAnalizar = cantLineas*porcentajeAnalisis;
+    pair<pair<int,int>,pair<int,int>> lineaActual;
+    int diagonalesCrecientes = 0;
+    int diagonalesDecrecientes = 0;
+    int verticales = 0;
+    int horizontales = 0;
+    int indiceRandom;
+    for(int contador = 0; contador < cantidadLineasAnalizar; contador++){
+        if(contador < cantidadLineasAnalizar/2){
+            indiceRandom = generarRandom(0,puntosSep.first.size());
+            lineaActual = puntosSep.first.at(indiceRandom);
+            if(lineaActual.first.first >= lineaActual.second.first-margenDeError && lineaActual.first.first <= lineaActual.second.first+margenDeError){
+                resultado.verticales++;
+            }
+            else if(lineaActual.first.first > lineaActual.second.first+margenDeError){
+                resultado.diagonalesCrecientes++;
+            }
+            else{
+                resultado.diagonalesDecrecientes++;
+            }
+            puntosSep.first.erase(puntosSep.first.begin()+contador);
+        }
+        else{
+            indiceRandom = generarRandom(0,puntosSep.second.size());
+            lineaActual = puntosSep.second.at(indiceRandom);
+            if(lineaActual.first.second >= lineaActual.second.second-margenDeError && lineaActual.first.second <= lineaActual.second.second+margenDeError){
+                resultado.horizontales++;
+            }
+            else if(lineaActual.first.second > lineaActual.second.second+margenDeError){
+                resultado.diagonalesCrecientes++;
+            }
+            else{
+                resultado.diagonalesDecrecientes++;
+            }
+            puntosSep.second.erase(puntosSep.second.begin()+contador);
+        }
     }
+    resultado.calcularTotales();
+    return resultado;
+}
+
+int calcularPromedioAux(vector<int> lista){
+    float suma = 0;
+    float listaSize = lista.size();
+    for(int contador = 0; contador < lista.size(); contador++){
+        suma += lista.at(contador);
+    }
+    return (int)suma/listaSize;
+}
+
+resultadosDeAnalisis calcularPromedio(unordered_map<int,resultadosDeAnalisis> ejecuciones){
+    vector<int> horizontales;
+    vector<int> verticales;
+    vector<int> diagonalesCrecientes;
+    vector<int> diagonalesDecrecientes;
+    for(int contador = 0; contador < ejecuciones.size(); contador++){
+         horizontales.push_back(ejecuciones.at(contador).horizontales);
+         verticales.push_back(ejecuciones.at(contador).verticales);
+         diagonalesCrecientes.push_back(ejecuciones.at(contador).diagonalesCrecientes);
+         diagonalesDecrecientes.push_back(ejecuciones.at(contador).diagonalesDecrecientes);
+    }
+
+    int promedioHorizontales = calcularPromedioAux(horizontales);
+    int promedioVerticales = calcularPromedioAux(verticales);
+    int promedioDiagonalesCrecientes = calcularPromedioAux(diagonalesCrecientes);
+    int promedioDiagonalesDecrecientes = calcularPromedioAux(diagonalesDecrecientes);
+
+    resultadosDeAnalisis resultadoFinal;
+    resultadoFinal.horizontales = promedioHorizontales;
+    resultadoFinal.verticales = promedioVerticales;
+    resultadoFinal.diagonalesCrecientes = promedioDiagonalesCrecientes;
+    resultadoFinal.diagonalesDecrecientes = promedioDiagonalesDecrecientes;
+    return resultadoFinal;
+
+}
+
+resultadosDeAnalisis realizarAnalisisCompleto(estructuraDePuntos puntos,float porcentajeAnalisis){
+    unordered_map<int,resultadosDeAnalisis> ejecuciones;
+    puntosSeparados puntosSep = separarPuntos(puntos);
+    for(int contador = 0; contador < 50; contador++){
+        resultadosDeAnalisis ejecucionActual = analizar(puntosSep,porcentajeAnalisis,puntos.size());
+        ejecuciones.insert({contador,ejecucionActual});
+    }
+    resultadosDeAnalisis resultado = calcularPromedio(ejecuciones);
+    return resultado;
 }
 
 estructuraDePuntos generarLineas(int cantidad){
@@ -102,25 +177,25 @@ estructuraDePuntos generarLineas(int cantidad){
     unordered_map<int,int> numerosUsadosY;
     while(cantidadHorizontales+cantidadVerticales < cantidad){
         if(cantidadHorizontales < cantidad/2){
-            int randomX = rand() % 1079;
+            int randomX = generarRandom(0,1079);
             if(numerosUsadosX.find(randomX) == numerosUsadosX.end()){
-                int randomEnd = (rand() % 15) - 15;
+                int randomEnd = generarRandom(-15,15);
                 resultado.push_back({{randomX,0},{randomX+randomEnd,919}});
                 cantidadHorizontales++;
                 numerosUsadosX.insert({randomX,1});
             }
             if(!cantidadPar && cantidadHorizontales == (cantidad/2)){
-                randomX = rand() % 1079;
-                int randomEnd = (rand() % 15) - 15;
+                int randomX = generarRandom(0,1079);
+                int randomEnd = generarRandom(-15,15);
                 resultado.push_back({{randomX,0},{randomX+randomEnd,919}});
                 cantidadHorizontales++;
                 numerosUsadosX.insert({randomX,1});
             }
         }
         else{
-            int randomY = rand() % 919;
+            int randomY = generarRandom(0,919);
             if(numerosUsadosY.find(randomY) == numerosUsadosY.end()){
-                int randomEnd = (rand() % 15) - 15;
+                int randomEnd = generarRandom(-15,15);
                 resultado.push_back({{0,randomY},{1079,randomY+randomEnd}});
                 cantidadVerticales++;
                 numerosUsadosY.insert({randomY,1});
@@ -138,14 +213,10 @@ void printPuntos(estructuraDePuntos puntos){
 }
 
 int main(){
-    estructuraDePuntos puntos = generarLineas(21);
-    puntosSeparados sepPuntos = separarPuntos(puntos);
-    puntosSeparados primerCuadrante = extraerCuadrante(0,250,0,160,&sepPuntos);
-    printPuntos(primerCuadrante.first);
-    printPuntos(primerCuadrante.second);
-
-    cout << endl;
-
-    printPuntos(sepPuntos.first);
-    printPuntos(sepPuntos.second);
+    estructuraDePuntos puntos = generarLineas(100);
+    resultadosDeAnalisis resultados = realizarAnalisisCompleto(puntos,0.6);
+    cout << "Horizontales: " << resultados.horizontales << endl;
+    cout << "Verticales: " << resultados.verticales << endl;
+    cout << "Diagonales crecientes: " << resultados.diagonalesCrecientes << endl;
+    cout << "Diagonales decrecientes: " << resultados.diagonalesDecrecientes << endl;
 }
